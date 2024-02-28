6e486c32b30db04df8b4f951d72a394c30b603de
Jason O'Neill <CreativeTechGuy@users.noreply.github.com>
2024-02-27 16:16:01 -0800

    Update change/quantity tooltips

--- src/main/java/com/adamk33n3r/runelite/watchdog/ui/alerts/InventoryAlertPanel.java
+++ src/main/java/com/adamk33n3r/runelite/watchdog/ui/alerts/InventoryAlertPanel.java
@@ -19,12 +19,12 @@ public class InventoryAlertPanel extends AlertPanel<InventoryAlert> {
             })
             .addIf(
                 panel -> panel.addRegexMatcher(this.alert, "Enter the name of the item to trigger on...", "The name to trigger on. Supports glob (*)")
-                    .addSpinner("Quantity", "The quantity of item to trigger on, use 0 for every time", this.alert.getItemQuantity(), this.alert::setItemQuantity, 0, Integer.MAX_VALUE, 1),
+                    .addSpinner("Quantity", "The minimum quantity of item to trigger on, use 0 for every time", this.alert.getItemQuantity(), this.alert::setItemQuantity, 0, Integer.MAX_VALUE, 1),
                 () -> this.alert.getInventoryAlertType() == InventoryAlert.InventoryAlertType.ITEM
             )
             .addIf(
                 panel -> panel.addRegexMatcher(this.alert, "Enter the name of the item to trigger on...", "The name to trigger on. Supports glob (*)")
-                    .addSpinner("Change", "The quantity change of the item (in one tick) to trigger on", this.alert.getItemQuantity(), this.alert::setItemQuantity, Integer.MIN_VALUE, Integer.MAX_VALUE, 1),
+                    .addSpinner("Change", "The quantity change of the item (in one tick) to trigger on, use 0 for no change", this.alert.getItemQuantity(), this.alert::setItemQuantity, Integer.MIN_VALUE, Integer.MAX_VALUE, 1),
                 () -> this.alert.getInventoryAlertType() == InventoryAlert.InventoryAlertType.ITEM_CHANGE
             )
             .addNotifications();
