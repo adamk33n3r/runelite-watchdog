abc3bdaab6e5ee1ca4cc7955fb93c0be03bfff59
MaeveMcT <maeve@mmctiernan.com>
2026-07-15 01:24:21 +0100

    Fix BankAlert registration indentation

--- src/main/java/com/adamk33n3r/runelite/watchdog/serialization/WatchdogGsonFactory.java
+++ src/main/java/com/adamk33n3r/runelite/watchdog/serialization/WatchdogGsonFactory.java
@@ -55,7 +55,7 @@ public class WatchdogGsonFactory {
             .registerSubtype(SoundFiredAlert.class)
             .registerSubtype(SpawnedAlert.class)
             .registerSubtype(InventoryAlert.class)
-                .registerSubtype(BankAlert.class)
+            .registerSubtype(BankAlert.class)
             .registerSubtype(AlertGroup.class)
             .registerSubtype(LocationAlert.class)
             .registerSubtype(AdvancedAlert.class);
